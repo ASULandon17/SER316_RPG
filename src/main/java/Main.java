d4e5aeddcfbe096e8344377bb32b274ac9bdbc68
Landon <dmessmor@asu.edu>
2024-02-19 22:16:13 -0800

    create comments and javadocs for enemies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,11 +17,14 @@ public class Main {
         //begin crawling dungeons
         while (gym < 6) {
             victorious = facade.generateDungeon(player, gym, level);
+            //if level is won
             if (victorious) {
                 level += 1;
                 facade.generateRandomItem(player);
+                //if gym is won
                 if (level > 10) {
                     gym += 1;
+                    //if game is won
                     if (gym == 6) {
                         gameWon = true;
                     } else {
@@ -29,18 +32,21 @@ public class Main {
                         TextBlocks.gymWon();
                         player = facade.generateTopFloor(player);
                     }
+                //if level is won but health is below 25%
                 } else if (player.getHealth() <= (player.getHitPoints() * 0.25)) {
                     TextBlocks.lowOnHealth(player);
                     player = facade.generateTopFloor(player);
                 }
             } else {
+              //if final battle is lost
                 if (gym == 5 && level == 10) {
-                    //player lost on the final boss
                     break;
                 }
+                //if battle is lost but its not the final battle
                 player = facade.generateTopFloor(player);
             }
         }
+        //if final battle has occurred
         facade.generateGameOverMessage(player, gameWon);
     }
 }
